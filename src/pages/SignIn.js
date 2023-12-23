f1bcad9b05e4351d9cc2d36e62f39b07bac30199
Tornike Despotashvili <despo@Tornikes-MacBook-Air.local>
2023-12-23 20:35:51 +0400

    redirect while user is not null

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,11 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import CreateButton from "../components/createButton/CreateButton";
 import { ProviderPass } from "../components/Provider";
 import * as signInStyle from "./styles/signin.module.css";
 import Sipnner from "../components/spinner/Sipnner";
+import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
-  const { authHandler, loading } = useContext(ProviderPass);
+  const { authHandler, loading, user, currentUser } = useContext(ProviderPass);
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    if(user || currentUser){
+      navigate('/pages/Evernote')
+    }
+  },[])
 
   return (
     <div className={signInStyle.signin}>
